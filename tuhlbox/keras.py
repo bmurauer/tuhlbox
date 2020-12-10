97c9a66fba1e83c278957e7cd8da64b04aa84a76
Benjamin Murauer <b.murauer@gmail.com>
2020-12-10 11:06:48 +0100

    Added Padder

--- tuhlbox/keras.py
+++ tuhlbox/keras.py
@@ -2,7 +2,8 @@
 import logging
 
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin
+from dstoolbox.transformers import Padder2d
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
@@ -12,6 +13,23 @@ logger = logging.getLogger(__name__)
 
 def count_num_words(x):
     return int(max([max(document) for document in x])) + 1
+
+
+class MaxLengthPadder(TransformerMixin, BaseEstimator):
+
+    def __init__(self, pad_value, min_length=5):
+        self.pad_value = pad_value
+        self.min_length = min_length
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, documents):
+        max_len = max([len(document) for document in documents]
+                      + [self.min_length])
+        print('max len is', max_len)
+        padder = Padder2d(max_len=max_len, pad_value=self.pad_value)
+        return padder.transform(documents)
 
 
 class KerasModel(BaseEstimator, ClassifierMixin):
