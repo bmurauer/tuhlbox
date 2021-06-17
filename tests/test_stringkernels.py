26172ffa4e40bf0b392752aca6c1ea47de4e69ff
Benjamin Murauer <b.murauer@gmail.com>
2021-06-17 14:56:36 +0200

    enable string kernels to be used in SVM class

--- tests/test_stringkernels.py
+++ tests/test_stringkernels.py
@@ -5,11 +5,11 @@ from tuhlbox.stringkernels import (intersection_kernel, presence_kernel,
                                    spectrum_kernel)
 
 docs = [
-    'I like this old movie. The movie is very nice.',
-    'In my opinion the book tells a very nice story. I really like it.',
-    'I wonder if you could drink this juice. It tastes so bad. Isn’t it bad?',
-    'Your dish is too spicy. You must be a such bad cook. '
-    'Don’t worry, I am as bad as you.',
+    "I like this old movie. The movie is very nice.",
+    "In my opinion the book tells a very nice story. I really like it.",
+    "I wonder if you could drink this juice. It tastes so bad. Isn’t it bad?",
+    "Your dish is too spicy. You must be a such bad cook. "
+    "Don’t worry, I am as bad as you.",
 ]
 
 ngram_min, ngram_max = 1, 4
@@ -20,12 +20,10 @@ def test_intersection_kernel() -> None:
     # obtained from:
     # java ComputeStringKernel intersection 1 4 sentences.txt <outfile>
     expected = np.array(
-        [[178, 95, 66, 49], [95, 254, 72, 72], [66, 72, 278, 112],
-         [49, 72, 112, 334]],
+        [[178, 95, 66, 49], [95, 254, 72, 72], [66, 72, 278, 112], [49, 72, 112, 334]],
         dtype=int,
     )
-    assert_array_equal(expected,
-                       intersection_kernel(docs, docs, ngram_min, ngram_max))
+    assert_array_equal(expected, intersection_kernel(ngram_min, ngram_max)(docs, docs))
 
 
 def test_presence_kernel() -> None:
@@ -34,12 +32,10 @@ def test_presence_kernel() -> None:
     # java ComputeStringKernel presence 1 4 sentences.txt <outfile>
 
     expected = np.array(
-        [[128, 67, 42, 29], [67, 197, 38, 42], [42, 38, 209, 64],
-         [29, 42, 64, 235]],
+        [[128, 67, 42, 29], [67, 197, 38, 42], [42, 38, 209, 64], [29, 42, 64, 235]],
         dtype=int,
     )
-    assert_array_equal(expected,
-                       presence_kernel(docs, docs, ngram_min, ngram_max))
+    assert_array_equal(expected, presence_kernel(ngram_min, ngram_max)(docs, docs))
 
 
 def test_spectrum_kernel() -> None:
@@ -55,5 +51,4 @@ def test_spectrum_kernel() -> None:
         ],
         dtype=int,
     )
-    assert_array_equal(expected,
-                       spectrum_kernel(docs, docs, ngram_min, ngram_max))
+    assert_array_equal(expected, spectrum_kernel(ngram_min, ngram_max)(docs, docs))
--- tuhlbox/stringkernels.py
+++ tuhlbox/stringkernels.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import logging
 from collections import defaultdict
-from typing import Any, Dict, List, Tuple
+from typing import Any, Callable, Dict, List, Tuple
 
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -11,90 +11,96 @@ from sklearn.base import BaseEstimator, TransformerMixin
 logger = logging.getLogger(__name__)
 
 
-def presence_kernel(
-    x: np.array, y: np.array, ngram_min: int, ngram_max: int
-) -> np.array:
+def presence_kernel(ngram_min: int, ngram_max: int) -> Callable:
     """Calculate the presence kernel, Ionescu & Popescu 2017."""
-    result = np.zeros((len(x), len(y)), dtype=int)
-    for i, string in enumerate(x):
-        if type(string) == str:
-            string = string.lower()
-        ngrams1 = set()
-        for ngram_size in range(ngram_min, ngram_max + 1):
-            for index in range(len(string) - ngram_size + 1):
-                ngram = string[index : index + ngram_size]
-                if type(ngram) != str:
-                    ngram = str(ngram)
-                ngrams1.add(ngram)
-        for j, counterstring in enumerate(y):
-            if type(counterstring) == str:
-                counterstring = counterstring.lower()
-            ngrams2 = set()
+
+    def internal_presence_kernel(x: np.array, y: np.array) -> np.array:
+        result = np.zeros((len(x), len(y)), dtype=int)
+        for i, string in enumerate(x):
+            if type(string) == str:
+                string = string.lower()
+            ngrams1 = set()
             for ngram_size in range(ngram_min, ngram_max + 1):
-                for index in range(len(counterstring) - ngram_size + 1):
-                    ngram = counterstring[index : index + ngram_size]
+                for index in range(len(string) - ngram_size + 1):
+                    ngram = string[index : index + ngram_size]
                     if type(ngram) != str:
                         ngram = str(ngram)
-                    ngrams2.add(ngram)
-            result[i, j] = len(ngrams1.intersection(ngrams2))
-    return result
+                    ngrams1.add(ngram)
+            for j, counterstring in enumerate(y):
+                if type(counterstring) == str:
+                    counterstring = counterstring.lower()
+                ngrams2 = set()
+                for ngram_size in range(ngram_min, ngram_max + 1):
+                    for index in range(len(counterstring) - ngram_size + 1):
+                        ngram = counterstring[index : index + ngram_size]
+                        if type(ngram) != str:
+                            ngram = str(ngram)
+                        ngrams2.add(ngram)
+                result[i, j] = len(ngrams1.intersection(ngrams2))
+        return result
+
+    return internal_presence_kernel
 
 
-def spectrum_kernel(
-    x: np.array, y: np.array, ngram_min: int, ngram_max: int
-) -> np.array:
+def spectrum_kernel(ngram_min: int, ngram_max: int) -> Callable:
     """Calculate the spectrum kernel, Ionescu & Popescu 2017."""
-    result = np.zeros((len(x), len(y)), dtype=int)
-    for i, string in enumerate(x):
-        if type(string) == str:
-            string = string.lower()
-        ngrams: Dict[str, int] = defaultdict(int)
-        for ngram_size in range(ngram_min, ngram_max + 1):
-            for index in range(len(string) - ngram_size + 1):
-                ngram = string[index : index + ngram_size]
-                if type(ngram) != str:
-                    ngram = str(ngram)
-                ngrams[ngram] += 1
-        for j, counterstring in enumerate(y):
-            if type(counterstring) == str:
-                counterstring = counterstring.lower()
+
+    def internal_spectral_kernel(x: np.array, y: np.array) -> np.array:
+        result = np.zeros((len(x), len(y)), dtype=int)
+        for i, string in enumerate(x):
+            if type(string) == str:
+                string = string.lower()
+            ngrams: Dict[str, int] = defaultdict(int)
             for ngram_size in range(ngram_min, ngram_max + 1):
-                for index in range(len(counterstring) - ngram_size + 1):
-                    ngram = counterstring[index : index + ngram_size]
+                for index in range(len(string) - ngram_size + 1):
+                    ngram = string[index : index + ngram_size]
                     if type(ngram) != str:
                         ngram = str(ngram)
-                    result[i, j] += ngrams[ngram]
-    return result
+                    ngrams[ngram] += 1
+            for j, counterstring in enumerate(y):
+                if type(counterstring) == str:
+                    counterstring = counterstring.lower()
+                for ngram_size in range(ngram_min, ngram_max + 1):
+                    for index in range(len(counterstring) - ngram_size + 1):
+                        ngram = counterstring[index : index + ngram_size]
+                        if type(ngram) != str:
+                            ngram = str(ngram)
+                        result[i, j] += ngrams[ngram]
+        return result
+
+    return internal_spectral_kernel
 
 
-def intersection_kernel(
-    x: np.array, y: np.array, ngram_min: int, ngram_max: int
-) -> np.array:
+def intersection_kernel(ngram_min: int, ngram_max: int) -> Callable:
     """Calculate the intersection kernel, Ionescu & Popescu 2017."""
-    result = np.zeros((len(x), len(y)), dtype=int)
-    for i, string in enumerate(x):
-        if type(string) == str:
-            string = string.lower()
-        ngrams: Dict[str, int] = defaultdict(int)
-        for ngram_size in range(ngram_min, ngram_max + 1):
-            for index in range(len(string) - ngram_size + 1):
-                ngram = string[index : index + ngram_size]
-                if type(ngram) != str:
-                    ngram = str(ngram)
-                ngrams[ngram] += 1
-        for j, counterstring in enumerate(y):
-            if type(counterstring) == str:
-                counterstring = counterstring.lower()
-            ngrams2 = dict(ngrams)
+
+    def internal_kernel(x: np.array, y: np.array) -> np.array:
+        result = np.zeros((len(x), len(y)), dtype=int)
+        for i, string in enumerate(x):
+            if type(string) == str:
+                string = string.lower()
+            ngrams: Dict[str, int] = defaultdict(int)
             for ngram_size in range(ngram_min, ngram_max + 1):
-                for index in range(len(counterstring) - ngram_size + 1):
-                    ngram = counterstring[index : index + ngram_size]
+                for index in range(len(string) - ngram_size + 1):
+                    ngram = string[index : index + ngram_size]
                     if type(ngram) != str:
                         ngram = str(ngram)
-                    if ngram in ngrams2 and ngrams2[ngram] > 0:
-                        result[i, j] += 1
-                        ngrams2[ngram] -= 1
-    return result
+                    ngrams[ngram] += 1
+            for j, counterstring in enumerate(y):
+                if type(counterstring) == str:
+                    counterstring = counterstring.lower()
+                ngrams2 = dict(ngrams)
+                for ngram_size in range(ngram_min, ngram_max + 1):
+                    for index in range(len(counterstring) - ngram_size + 1):
+                        ngram = counterstring[index : index + ngram_size]
+                        if type(ngram) != str:
+                            ngram = str(ngram)
+                        if ngram in ngrams2 and ngrams2[ngram] > 0:
+                            result[i, j] += 1
+                            ngrams2[ngram] -= 1
+        return result
+
+    return internal_kernel
 
 
 kernel_map = {
@@ -114,10 +120,10 @@ class StringKernelTransformer(BaseEstimator, TransformerMixin):
     """
 
     def __init__(
-            self,
-            kernel_type: str = "intersection",
-            ngram_range: Tuple[int, int] = None,
-            normalize: bool = True
+        self,
+        kernel_type: str = "intersection",
+        ngram_range: Tuple[int, int] = None,
+        normalize: bool = True,
     ):
         """
         Initialize the model.
@@ -141,7 +147,7 @@ class StringKernelTransformer(BaseEstimator, TransformerMixin):
     def fit(self, X: List[str], _y: Any = None) -> StringKernelTransformer:
         """Fit model."""
         self._train_data = np.array(X)
-        self._train_kernel = self._kernel(X, X, *self.ngram_range)
+        self._train_kernel = self._kernel(*self.ngram_range)(X, X)
         for i in range(len(X)):
             if self._train_kernel[i, i] == 0:
                 logger.error(f"zeros in diagonal at ({i},{i}) for {X[i]}")
@@ -150,13 +156,13 @@ class StringKernelTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X: List[str], _y: Any = None) -> np.array:
         """Transform data."""
         X = np.array(X)
-        _st = self._kernel(X, self._train_data, *self.ngram_range)
+        _st = self._kernel(*self.ngram_range)(X, self._train_data)
 
         if not self.normalize:
             return _st
 
         _ss = self._train_kernel
-        _tt = self._kernel(X, X, *self.ngram_range)
+        _tt = self._kernel(*self.ngram_range)(X, X)
 
         result = np.copy(_st)
         for i in range(_st.shape[0]):
