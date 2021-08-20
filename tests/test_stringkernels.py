f99bbb8284ed0950cf105747fbd7dfac7232e557
Benjamin Murauer <b.murauer@gmail.com>
2021-08-20 10:17:52 +0200

    release v0.4.9: Enable non-string svm kernels

--- tests/test_stringkernels.py
+++ tests/test_stringkernels.py
@@ -1,16 +1,16 @@
 """Test String-Kernel based models."""
 import numpy as np
+from dstoolbox.transformers import TextFeaturizer
 from numpy.testing import assert_array_equal
 from tuhlbox.stringkernels import (intersection_kernel, presence_kernel,
-                                   spectrum_kernel, legacy_intersection_kernel,
-                                   legacy_spectrum_kernel, legacy_presence_kernel)
+                                   spectrum_kernel)
 
 docs = [
-    "I like this old movie. The movie is very nice.",
-    "In my opinion the book tells a very nice story. I really like it.",
-    "I wonder if you could drink this juice. It tastes so bad. Isn’t it bad?",
-    "Your dish is too spicy. You must be a such bad cook. "
-    "Don’t worry, I am as bad as you.",
+    "i like this old movie. the movie is very nice.",
+    "in my opinion the book tells a very nice story. i really like it.",
+    "i wonder if you could drink this juice. it tastes so bad. isn’t it bad?",
+    "your dish is too spicy. you must be a such bad cook. "
+    "don’t worry, i am as bad as you.",
 ]
 
 ngram_min, ngram_max = 1, 4
@@ -24,44 +24,23 @@ def test_intersection_kernel() -> None:
         [[178, 95, 66, 49], [95, 254, 72, 72], [66, 72, 278, 112], [49, 72, 112, 334]],
         dtype=int,
     )
-    assert_array_equal(expected, intersection_kernel(ngram_min, ngram_max)(docs, docs))
 
-
-def test_legacy_intersection_kernel() -> None:
-    """Test intersection kernel by comparing with original code."""
-    # obtained from:
-    # java ComputeStringKernel intersection 1 4 sentences.txt <outfile>
-    expected = np.array(
-        [[178, 95, 66, 49], [95, 254, 72, 72], [66, 72, 278, 112], [49, 72, 112, 334]],
-        dtype=int,
-    )
-    actual = legacy_intersection_kernel(ngram_min, ngram_max)(docs, docs)
-    assert_array_equal(expected, actual)
+    tf = TextFeaturizer(analyzer='char', ngram_range=(ngram_min, ngram_max))
+    ngrams = tf.fit_transform(docs)
+    assert_array_equal(expected, intersection_kernel(ngrams, ngrams))
 
 
 def test_presence_kernel() -> None:
     """Test presence kernel by comparing with original code."""
     # obtained from:
     # java ComputeStringKernel presence 1 4 sentences.txt <outfile>
-
     expected = np.array(
         [[128, 67, 42, 29], [67, 197, 38, 42], [42, 38, 209, 64], [29, 42, 64, 235]],
         dtype=int,
     )
-    assert_array_equal(expected, presence_kernel(ngram_min, ngram_max)(docs, docs))
-
-
-def test_legacy_presence_kernel() -> None:
-    """Test presence kernel by comparing with original code."""
-    # obtained from:
-    # java ComputeStringKernel presence 1 4 sentences.txt <outfile>
-
-    expected = np.array(
-        [[128, 67, 42, 29], [67, 197, 38, 42], [42, 38, 209, 64], [29, 42, 64, 235]],
-        dtype=int,
-    )
-    assert_array_equal(expected, legacy_presence_kernel(ngram_min, ngram_max)(docs, docs))
-
+    tf = TextFeaturizer(analyzer='char', ngram_range=(ngram_min, ngram_max))
+    ngrams = tf.fit_transform(docs)
+    assert_array_equal(expected, presence_kernel(ngrams, ngrams))
 
 
 def test_spectrum_kernel() -> None:
@@ -77,20 +56,26 @@ def test_spectrum_kernel() -> None:
         ],
         dtype=int,
     )
-    assert_array_equal(expected, spectrum_kernel(ngram_min, ngram_max)(docs, docs))
+    tf = TextFeaturizer(analyzer='char', ngram_range=(ngram_min, ngram_max))
+    ngrams = tf.fit_transform(docs)
+    assert_array_equal(expected, spectrum_kernel(ngrams, ngrams))
 
 
-def test_legacy_spectrum_kernel() -> None:
-    """Test spectrum kernel by comparing with original code."""
-    # obtained from:
-    # java ComputeStringKernel spectrum 1 4 sentences.txt <outfile>
-    expected = np.array(
-        [
-            [390, 335, 300, 313],
-            [335, 598, 393, 458],
-            [300, 393, 680, 585],
-            [313, 458, 585, 1006],
-        ],
-        dtype=int,
-    )
-    assert_array_equal(expected, legacy_spectrum_kernel(ngram_min, ngram_max)(docs, docs))
+def test_kernels_with_non_strings() -> None:
+    """Tests the presence kernel with documents not consisting of strings."""
+
+    ngrams = [
+        [1, 2, 3, 3, 2],
+        [2, 3, 3, 1, 4],
+        [6, 7, 5, 1, 2]
+    ]
+
+    expected = np.array([
+        [3, 3, 2],  # the first document has 4 elements common to itself,
+                    # 4 elements in common with the second document
+                    # and 2 documents in common with the last document.
+        [3, 4, 2],
+        [2, 2, 5]
+    ])
+    actual = presence_kernel(ngrams, ngrams)
+    assert_array_equal(expected, actual)
